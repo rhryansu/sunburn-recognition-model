44a31e1d1a9f6e5f708c289ef0a03e75849801c4
Ryan-Win <rh.ryan.su@gmail.com>
2022-05-02 16:51:34 +1000

    v2.0 fix model incorrect problem

--- app_v2.py
+++ app_v2.py
@@ -6,6 +6,7 @@ from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 import numpy as np
 import os 
+from tensorflow.keras.preprocessing import image
   
 #try:
 #    import shutil
@@ -19,35 +20,40 @@ model = tf.keras.models.load_model('model')
 app = Flask(__name__)
   
 app.config['UPLOAD_FOLDER'] = 'uploaded/image'
+save_pic_name = 'one_img.png'
+img_path = os.path.join(app.config['UPLOAD_FOLDER'], save_pic_name)
   
 @app.route('/')
 def upload_f():
     return render_template('upload.html')
   
 def finds():
-    test_datagen = ImageDataGenerator(rescale = 1./255)
+    # test_datagen = ImageDataGenerator(rescale = 1./255)
     vals = ["Second_degree", "First_degree"]
-    test_dir = 'uploaded'
-    test_generator = test_datagen.flow_from_directory(
-            test_dir,
-            target_size =(255, 255),
-            color_mode ="rgb",
-            shuffle = False,
-            class_mode ='categorical',
-            batch_size = 1)
+    # test_dir = 'uploaded'
+    # test_generator = test_datagen.flow_from_directory(
+    #         test_dir,
+    #         target_size =(255, 255),
+    #         color_mode ="rgb",
+    #         shuffle = False,
+    #         class_mode ='categorical',
+    #         batch_size = 1)
+    # img_path = os.path.join(app.config['UPLOAD_FOLDER'], save_pic_path)
+    # img_path = '/content/app/uploaded/image/WX20220424-1754202x.png'
+    img = image.load_img(img_path, target_size=(64, 64))
+    x = image.img_to_array(img)/255.0
+    x = np.expand_dims(x, axis=0)
   
-    pred = model.predict_generator(test_generator)
+    # pred = model.predict_generator(test_generator)
+    pred = model.predict(x)
     print(pred)
-    print(np.argmax(pred,axis = 1))
-    indexs = np.argmax(pred,axis = 1)
-    print(vals)
-    return str(vals[indexs[0]])
+    return str(vals[np.argmax(pred)])
   
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        f.save(img_path)
         val = finds()
         return render_template('pred.html', ss = val)
   
